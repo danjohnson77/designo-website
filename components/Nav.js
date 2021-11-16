372e95a8b7d193146945c10a40c500b7a91963ac
Daniel Johnson <danjohnson77@gmail.com>
2021-11-16 12:18:01 -0800

    nav aria fix

--- components/Nav.js
+++ components/Nav.js
@@ -8,26 +8,35 @@ const Nav = ({ styleProp = null, handleClick, header = false }) => {
     >
       <ul>
         <li>
-          <div id="nav-label-1" style={{ position: "absolute", top: "-9999px", left: "-9999px" }}>
+          <div
+            id={`${header ? "header-label-1" : "footer-label-1"}`}
+            style={{ position: "absolute", top: "-9999px", left: "-9999px" }}
+          >
             About Page
           </div>
-          <a href="/about" aria-label="nav-label-1">
+          <a href="/about" aria-label={`${header ? "header-label-1" : "footer-label-1"}`}>
             OUR COMPANY
           </a>
         </li>
         <li>
-          <div id="nav-label-2" style={{ position: "absolute", top: "-9999px", left: "-9999px" }}>
+          <div
+            id={`${header ? "header-label-2" : "footer-label-2"}`}
+            style={{ position: "absolute", top: "-9999px", left: "-9999px" }}
+          >
             Locations Page
           </div>
-          <a href="/locations" aria-label="nav-label-2">
+          <a href="/locations" aria-label={`${header ? "header-label-2" : "footer-label-2"}`}>
             LOCATIONS
           </a>
         </li>
         <li>
-          <div id="nav-label-3" style={{ position: "absolute", top: "-9999px", left: "-9999px" }}>
+          <div
+            id={`${header ? "header-label-3" : "footer-label-3"}`}
+            style={{ position: "absolute", top: "-9999px", left: "-9999px" }}
+          >
             Contact Page
           </div>
-          <a href="/contact" aria-label="nav-label-3">
+          <a href="/contact" aria-label={`${header ? "header-label-3" : "footer-label-3"}`}>
             CONTACT
           </a>
         </li>
